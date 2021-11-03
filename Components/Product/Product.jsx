83fbc3345b1b8a8d488742f51059339a6f21e753
ojoaoguilherme <joaoguilherme.pinheiro@outlook.com>
2021-11-03 12:38:40 -0300

    structered text aligned

--- Components/Product/Product.jsx
+++ Components/Product/Product.jsx
@@ -4,18 +4,22 @@ import ProductStyle from "./ProductStyle";
 import { StructuredText } from "react-datocms";
 import { Image } from "react-datocms";
 
-
 export default function Product({ product }) {
-  console.log(product)
+  console.log(product);
   return (
     <ProductStyle>
-      <Image
-        data={product.image.responsiveImage}
-        alt={product.image.responsiveImage.alt}
-      />
-      <h3>{product.name} - R${product.price}</h3>
-      <StructuredText data={product.description}/>
-
+      <div className='image-container'>
+        <Image
+          data={product.image.responsiveImage}
+          alt={product.image.responsiveImage.alt}
+        />
+      </div>
+      <div className="text-content">
+        <h3>
+          {product.name} - R${product.price}
+        </h3>
+        <StructuredText data={product.description.value} />
+      </div>
     </ProductStyle>
   );
 }
--- Components/Product/ProductStyle.js
+++ Components/Product/ProductStyle.js
@@ -1,10 +1,37 @@
 import styled from "styled-components";
 
 const ProductStyle = styled.div`
-  h3 {
-    margin: 7px;
-    text-align: center;
-    font-size: 25px;
+
+  .image-container{
+    margin: 25px;
+    img{
+      border-radius: 10px;
+    }
+
+  }
+
+  .text-content {
+    margin-left: 10px;
+    h3 {
+      margin: 7px;
+      text-align: center;
+      font-size: 25px;
+    }
+    ol {
+      padding-left: 25px;
+    }
+    ul {
+      padding-left: 25px;
+    }
+    blockquote {
+      margin: 3px;
+      margin-left: 25px;
+      border-left: 4px solid black;
+      p {
+        padding: 7px;
+      }
+      color: gray;
+    }
   }
 `;
 
