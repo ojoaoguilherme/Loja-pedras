dabcfd90075f5c75ece467b2622b87941dd81bf6
ojoaoguilherme <joaoguilherme.pinheiro@outlook.com>
2021-11-11 15:45:53 -0300

    order linked to a product done

--- Components/PayButton/PayButton.jsx
+++ Components/PayButton/PayButton.jsx
@@ -1,25 +1,45 @@
 import React from "react";
 import { PayPalButton } from "react-paypal-button-v2";
+
+//API Dato
 import createRecord from "../../lib/orderAPI";
+import publishProductStatus from "../../lib/publishProduct";
 import updateProductStatus from "../../lib/updateProduct";
 
 export default function PayButton({ product }) {
   return (
     <PayPalButton
-      amount={product.price}
-      // shippingPreference="NO_SHIPPING" // default is "GET_FROM_FILE"
-      onSuccess={(details, data) => {
-        createRecord(details);
-        updateProductStatus(product);
-        alert("Transação efetuada com sucesso!!"); // + details.payer.name.given_name
-
-        // OPTIONAL: Call your server to save the transaction
-      }}
       options={{
         clientId:
           "AYJk9uaMyv9pSmvXQ3qmzieNElP3nXeThJI_awjMen30Q2JjRzKbLvcuo8cNs6IcRvZleeE72NhIbkhE" ||
           `${process.env.PRODUCTION_CLIENT_ID}`,
         currency: "BRL",
+      }}
+      currency="BRL"
+      createOrder={(data, actions) => {
+        return actions.order.create({
+          purchase_units: [
+            {
+              reference_id: product.id,
+              description: product.name,
+
+              amount: {
+                value: product.price,
+                currency: "BRL",
+              },
+            },
+          ],
+        });
+      }}
+      onApprove={(data, actions) => {
+        // Capture the funds from the transaction
+        return actions.order.capture().then(function (details) {
+          // Show a success message to your buyer
+          alert("Transaction completed by " + details.payer.name.given_name);
+          createRecord(details);
+          updateProductStatus(product);
+          publishProductStatus(product);
+        });
       }}
     />
   );
